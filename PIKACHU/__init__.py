b981e0f89906678adab5ce2df9de10d7f18f9d7f
FuFu <fufu.bluesand@gmail.com>
2018-08-31 14:43:30 +0800

    change version

--- PIKACHU/__init__.py
+++ PIKACHU/__init__.py
@@ -2,5 +2,5 @@ from PIKACHU.producor import SimpleProducor, BroadCaster
 from PIKACHU.consumer import SimpleConsumer, SimpleAsyncConsumer, Receiver
 
 name = "PI-KA-CHU"
-version = "0.2.1"
+version = "0.2.2"
 author  = "fufu"
